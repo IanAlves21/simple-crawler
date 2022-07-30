3905b104621d203a8149d8ae7a5be3ae234cc489
Ian Silva <ian.alves21@gmail.com>
2022-07-30 12:32:11 -0400

    feat: Create crawler class file with basic methods

---
+++ src/crawler/SimpleCrawler.py
@@ -0,0 +1,14 @@
+class SimpleCrawler:
+    # Constructor
+    def __init__(self):
+        print("initializing")
+
+    # Public methods
+    def print(self) -> None:
+        print("print")
+
+    def save_csv(self) -> None:
+        print("save_csv")
+
+    def save_json(self) -> None:
+        print("save_json")
