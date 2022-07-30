bd4ac15653a08fe188f69e9c5c49e1cf133c1bda
Ian Silva <ian.alves21@gmail.com>
2022-07-30 13:12:15 -0400

    chore: Update main crawler class methods

--- src/crawler/SimpleCrawler.py
+++ src/crawler/SimpleCrawler.py
@@ -1,7 +1,15 @@
+import requests
+
+from bs4 import BeautifulSoup
+
+
 class SimpleCrawler:
     # Constructor
-    def __init__(self):
+    def __init__(self, url: str):
         print("initializing")
+        self.url = url
+        self.site_raw_data = self.__request_data()
+        self.site_structured_data = dict()
 
     # Public methods
     def print(self) -> None:
@@ -12,3 +20,11 @@ class SimpleCrawler:
 
     def save_json(self) -> None:
         print("save_json")
+
+    # Private methods
+    def __request_data(self) -> BeautifulSoup:
+        print("request_for_data")
+        html = requests.get(self.url)
+        soup_element = BeautifulSoup(html.content, 'html.parser')
+
+        return soup_element
